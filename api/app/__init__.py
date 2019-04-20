4c9ed7e24d95b31c123e889cf427d9f540273d72
Royston Yinkore <royston.yinkore@kpmg.co.uk>
2019-04-20 08:50:06 +0100

    Added graphql

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_graphql import GraphQLView
+
 
 def create_app():
     app = Flask(__name__)
@@ -6,5 +8,15 @@ def create_app():
     @app.route("/")
     def hello_world():
         return "Hello World!"
+
+    from app.schema import schema
+    app.add_url_rule(
+        '/graphql',
+        view_func=GraphQLView.as_view(
+            'graphql',
+            schema=schema,
+            graphiql=True  # for having the GraphiQL interface
+        )
+    )
 
     return app
