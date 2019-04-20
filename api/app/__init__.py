6c697253f309d33c10601dd97ba76c424cc5df2e
Royston Yinkore <royston.yinkore@kpmg.co.uk>
2019-04-20 10:28:54 +0100

    Added cors

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_graphql import GraphQLView
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     
     @app.route("/")
     def hello_world():
