1684c8dfab8cdab87219d796d5f6beab60dbab1a
Royston Yinkore <roystonyinkore@gmail.com>
2019-04-21 10:59:17 +0100

    Simple query to get users

--- api/app/schema.py
+++ api/app/schema.py
@@ -1,13 +1,51 @@
-from graphene import ObjectType, String, Schema
+import graphene
 
-class ExampleQuery(ObjectType):
-    hello = String()
-    
-    def resolve_hello(self, info):
-        return "Hello world"
+db = {}
+db['users'] = [
+    {
+        'id': 'something',
+        'username': 'Royston',
+        'password': 'hash'
+    },
+    {
+        'id': 'something1',
+        'username': 'Korede',
+        'password': 'hash'
+    },
+    {
+        'id': 'something2',
+        'username': 'Toyosi',
+        'password': 'hash'
+    }
+]
 
-class RootQuery(ExampleQuery, ObjectType):
+
+class GetUserModel(graphene.ObjectType):
+    _id = graphene.String()
+    username = graphene.String()
+
+
+class CreateUserModel(graphene.ObjectType):
+    user_name = graphene.String()
+    password = graphene.String()
+
+
+class UsersQuery(graphene.ObjectType):
+    users = graphene.List(GetUserModel)
+
+    def resolve_users(self, info):
+        return [GetUserModel(
+            _id = user['id'],
+            username = user['username']
+        ) for user in db['users']]
+
+
+class RootQuery(UsersQuery):
     pass
 
 
-schema = Schema(query=RootQuery)
+"""class RootMutation(graphene.ObjectType):
+    create_user = CreateUser.Field()"""
+
+
+schema = graphene.Schema(query=RootQuery)
