c4081338aeebf624fdf7e56cb5682de1bc2c9173
Royston Yinkore <roystonyinkore@gmail.com>
2019-04-21 11:41:24 +0100

    Added other CRUD options

--- api/app/schema.py
+++ api/app/schema.py
@@ -1,51 +1,86 @@
 import graphene
+import uuid
 
 db = {}
-db['users'] = [
+db['users'] = [{**user, 'id': str(uuid.uuid4())}
+for user in [
     {
-        'id': 'something',
         'username': 'Royston',
         'password': 'hash'
     },
     {
-        'id': 'something1',
         'username': 'Korede',
         'password': 'hash'
     },
     {
-        'id': 'something2',
         'username': 'Toyosi',
         'password': 'hash'
     }
-]
+]]
 
 
-class GetUserModel(graphene.ObjectType):
-    _id = graphene.String()
+class User(graphene.ObjectType):
+    _id = graphene.ID()
     username = graphene.String()
 
 
-class CreateUserModel(graphene.ObjectType):
+class CreateUser(graphene.Mutation):
+    class Arguments:
+        user_name = graphene.String()
+        password = graphene.String()
+
+    _id = graphene.ID()
     user_name = graphene.String()
-    password = graphene.String()
+
+    def mutate(self, info, user_name, password):
+        new_user = {
+            'id': str(uuid.uuid4()),
+            'username': user_name,
+            'password': password
+        } 
+
+        db['users'].append(new_user)
+
+        return CreateUser(
+            _id=new_user['id'],
+            user_name=new_user['username']
+        )
+
+
+class DeleteUser(graphene.Mutation):
+    class Arguments:
+        _id = graphene.ID()
+
+    ok = graphene.Boolean()
+
+    def mutate(self, info, _id):
+        db['users'] = [user for user in db['users'] if user['id'] != _id]
+        return DeleteUser(ok=True)
 
 
 class UsersQuery(graphene.ObjectType):
-    users = graphene.List(GetUserModel)
+    users = graphene.List(User, id=graphene.ID())
 
-    def resolve_users(self, info):
-        return [GetUserModel(
-            _id = user['id'],
-            username = user['username']
-        ) for user in db['users']]
+    def resolve_users(self, info, **kwargs):
+        _id = kwargs.get('id', None)
+
+        if _id:
+            return [
+                User(_id=user['id'], username=user['username']) 
+                for user in db['users']
+                if user['id'] == _id
+            ]
+
+        return [User(_id=user['id'], username=user['username']) for user in db['users']]
 
 
 class RootQuery(UsersQuery):
     pass
 
 
-"""class RootMutation(graphene.ObjectType):
-    create_user = CreateUser.Field()"""
+class RootMutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+    delete_user = DeleteUser.Field()
 
 
-schema = graphene.Schema(query=RootQuery)
+schema = graphene.Schema(query=RootQuery, mutation=RootMutation)
